a8ad6ecc1cf4ac99bbd5b8dfc452dd21482df76b
olliem36 <olivermahoney@me.com>
2017-11-06 14:06:08 +0000

    building create wallet views

--- app/src/main/java/com/mobileappyness/crypto/eltwallet/NewWalletActivity.kt
+++ app/src/main/java/com/mobileappyness/crypto/eltwallet/NewWalletActivity.kt
@@ -13,10 +13,7 @@ class NewWalletActivity : AppCompatActivity() {
         setContentView(R.layout.activity_new_wallet)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
+
     }
 
 }
